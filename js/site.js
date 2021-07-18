188c35a516b7813b90eb89ec0c0aa42b130e24cb
Charlie Kaylor <68200523+ckaylor33@users.noreply.github.com>
2021-07-18 18:28:29 +0100

    Added alternate fizzBuzz logic solutions

--- js/site.js
+++ js/site.js
@@ -12,7 +12,7 @@ function getValues() {
   //check that the numbers are integers
   if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
     //call fizzBuzz
-    let fbArray = fizzBuzz(fizzValue, buzzValue)
+    let fbArray = fizzBuzzThree(fizzValue, buzzValue)
     //call displayData and write values to the screen
     displayData(fbArray)
   } else {
@@ -39,6 +39,54 @@ function fizzBuzz(fizzValue, buzzValue) {
     } else {
       returnArray.push(i)
     }
+  }
+
+  return returnArray
+}
+
+//Alternate Option
+function fizzBuzzTwo(fizzValue, buzzValue) {
+  let returnArray = []
+
+  let Fizz = false
+  let Buzz = false
+
+  for (let i = 1; i < 100; i++) {
+    Fizz = i % fizzValue == 0
+    Buzz = i % buzzValue == 0
+
+    switch (true) {
+      case Fizz && Buzz: {
+        returnArray.push('FizzBuzz')
+        break
+      }
+      case Fizz: {
+        returnArray.push('Fizz')
+        break
+      }
+      case Buzz: {
+        returnArray.push('Buzz')
+        break
+      }
+      default: {
+        returnArray.push(i)
+        break
+      }
+    }
+  }
+
+  return returnArray
+}
+
+//Alternate Option
+function fizzBuzzThree(fizzValue, buzzValue) {
+  let returnArray = []
+
+  for (let i = 1; i < 100; i++) {
+    let value =
+      (i % fizzValue == 0 ? 'Fizz' : '') + (i % buzzValue == 0 ? 'Buzz' : '') ||
+      i
+    returnArray.push(value)
   }
 
   return returnArray
